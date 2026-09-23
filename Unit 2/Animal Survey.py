960a05a37b288ab3eecd7286babf10dede1672a9
DevinBaxter29 <836418@mystma.org>
2026-09-23 13:34:48 -0500

    Animal Survey

--- Unit 2/Animal Survey.py
+++ Unit 2/Animal Survey.py
@@ -1,11 +1,11 @@
-favorite_animal = input("What is Your Favorite Animal?\n> ")
+favorite_animal = input("What is Your Favorite Animal?\n> ")        #Get Favorite Aniaml
 
-why_favorite = input("Why is it Your Favorite Animal?\n> ")
+why_favorite = input("Why is it Your Favorite Animal?\n> ")         #Get Why Favorite
 
-fun_fact = input("Whats A Fun Fact About It?\n> ")
+fun_fact = input("Whats A Fun Fact About It?\n> ")                  #Get Fun Fact
 
-location = input("Where Do They Live?\n> ")
+location = input("Where Do They Live?\n> ")                         #Get Location
 
-diet = input("What Does It Eat?\n> ")
+diet = input("What Does It Eat?\n> ")                               #Get Diet Of Favorite animal
 
-print("Your favorite animal is a " + favorite_animal, "because " + why_favorite, "a fun fact is " + fun_fact, "it lives " + location, "it eats " + diet)
+print("Your favorite animal is a " + favorite_animal, "because " + why_favorite + ".", "A fun fact is " + fun_fact, "it lives " + location, "it eats " + diet + ".")
