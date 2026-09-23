5747b9d3c2ae48059240275fdcca201e6d1b5c0a
DevinBaxter29 <836418@mystma.org>
2026-09-23 13:49:05 -0500

    Practice

--- Unit 2/2.3-Practice.py
+++ Unit 2/2.3-Practice.py
@@ -1,2 +1,6 @@
-fav_color = input("What is your favorite color?")
+#fav_color = input("What is your favorite color?") 
+#I Removed other lines to see what this line did ^
 
+num = input("Enter A Number\n> ")
+num = int(num)
+print(num * 3)
