b83c1bfcea14919e4d84e915f042303d95862a41
DevinBaxter29 <836418@mystma.org>
2026-09-23 13:21:04 -0500

    Space Shuttle

---
+++ Unit 2/Space Shuttle Supply Check.py
@@ -0,0 +1,20 @@
+
+countdown = input("How Many Seconds Until Launch?\n> ")
+
+oxygen_tanks = input("How Many Oxygen Tanks Do You Have?\n> ")
+
+food_packs = input("Number of Food Packs for Journey\n> ")
+
+water_packs = input("How many water packs are stored on the ship?\n> ")
+
+print("Countdown: " + countdown)
+print("Oxygen Tanks: " + oxygen_tanks)
+print("Food Packs: " + food_packs)
+print("Water Packs: " + oxygen_tanks)
+
+oxygen_tanks = input("Confirm Number Of Oxygen Tanks Packed\n>")
+
+print("Countdown: " + countdown)
+print("Oxygen Tanks: " + oxygen_tanks)
+print("Food Packs: " + food_packs)
+print("Water Packs: " + oxygen_tanks)
