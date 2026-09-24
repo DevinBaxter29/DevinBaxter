df205da4de68113f57cc6b1dfbcf595750bfd40f
DevinBaxter29 <836418@mystma.org>
2026-09-24 13:55:01 -0500

    Mad Lib

--- Unit 2/SimpleMadLib.py
+++ Unit 2/SimpleMadLib.py
@@ -1,8 +1,8 @@
 # One time, in a [adjective_1] [noun_1],
 # there was a [adjective_2] [noun_2].
-                                                         # Every night, the [noun_3] would [verb_1] [adverb_1].
-                                                        # One morning, a [adjective_3] [noun_4] appeared
-                                                                                                                                            # and [verb_2] the [noun_5] away.
+# Every night, the [noun_3] would [verb_1] [adverb_1].
+# One morning, a [adjective_3] [noun_4] appeared
+# and [verb_2] the [noun_5] away.
 
 adjective_1 = input("Enter an adjective: ")  #Get adjective from User
 adjective_2 = input("Enter an adjective: ")  #Get adjective from User
