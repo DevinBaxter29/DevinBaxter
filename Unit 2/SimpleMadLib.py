c15f4bf1bfb6ef5381e9e91b512419da2b1d9208
DevinBaxter29 <836418@mystma.org>
2026-09-24 13:50:05 -0500

    Mad Lib

---
+++ Unit 2/SimpleMadLib.py
@@ -0,0 +1,22 @@
+# One time, in a [adjective_1] [noun_1],
+# there was a [adjective_2] [noun_2].
+                                                         # Every night, the [noun_3] would [verb_1] [adverb_1].
+                                                        # One morning, a [adjective_3] [noun_4] appeared
+                                                                                                                                            # and [verb_2] the [noun_5] away.
+
+adjective_1 = input("Enter an adjective: ")
+adjective_2 = input("Enter an adjective: ")
+adjective_3 = input("Enter an adjective: ")
+
+noun_1 = input("Enter a noun: ")
+noun_2 = input("Enter a noun: ")
+noun_3 = input("Enter a noun: ")
+noun_4 = input("Enter a noun: ")
+noun_5 = input("Enter a noun: ")
+
+verb_1 = input("Enter a verb: ")
+verb_2 = input("Enter a verb: ")
+
+adverb_1 = input("Enter an adverb: ")
+
+print("One time, in a " + adjective_1, noun_1, "there was a " + adjective_2, noun_2, "Every night, the " + noun_3, "would ", verb_1, adverb_1,  "One morning, a " + adjective_3, noun_4, "appeared and " + verb_2, "the ", noun_5, "away." )
